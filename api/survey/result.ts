1d699a8ff9943187da50f66ba84c13fa9d09b2e6
ChunKam Yu <ohhhcky@outlook.com>
2023-08-06 17:00:13 -0700

    feat(api/survey/result.ts): add survey results outputting API

---
+++ api/survey/result.ts
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from 'uuid';
+import checkToken from '~/src/functions/checkToken';
+export default eventHandler(async (event) => {
+    const storage = useStorage()
+    const { uniqueId, userId, token, } = await readBody(event)
+    const _ans: object = await storage.getItem('ans')
+    if (!uniqueId) {
+        return {
+            code: 1001,
+            msg: 'Invalid parameters.'
+        }
+    }
+    if (!token || !await checkToken(token)) {
+        return {
+            code: 2001,
+            msg: 'Invalid token.'
+        }
+    }
+    const ans = _ans[uniqueId]
+    if (!ans) {
+        return {
+            code: 2001,
+            msg: 'Database Error.'
+        }
+    }
+    const survey: object = await storage.getItem('sid')
+    const svId = (survey || []).find(s => s.id == uniqueId)
+    if (!survey || !svId) {
+        return {
+            code: 2001,
+            msg: 'Database Error.'
+        }
+    }
+    let answers;
+    if (userId) {
+        answers = ans.filter(a => a.usr == userId)
+    } else {
+        answers = ans
+    }
+    return {
+        code: 0,
+        msg: 'Success.',
+        answers,
+        survey: svId,
+    }
+})
+  
