ef271b5732cd449f2cb28b91f91cf02d471a1918
ChunKam Yu <ohhhcky@outlook.com>
2023-08-19 21:41:15 -0700

    chore: delete `routes` dir

--- routes/_ans_test.ts
+++
@@ -1,6 +0,0 @@
-export default eventHandler(async () => {
-	// await storage.setItem("foo:bar", "baz");
-	const storage = useStorage("cransurvey")
-	const ans: object = await storage.getItem("ans")
-	return ans
-})
--- routes/index.ts
+++
@@ -1,4 +0,0 @@
-export default eventHandler(() => {
-	// await storage.setItem("foo:bar", "baz");
-	return { CranSurvey: "🗒️ Add a prompt window to get surveys from your website." }
-})
