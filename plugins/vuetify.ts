137b1f73a422f194b654b49b225610aaf8c2c47e
iColaBot <icolabot@e.yfun.top>
2023-09-09 18:05:41 +0000

    style(eslint): update

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,7 +1,7 @@
 import { createVuetify } from "vuetify"
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
-import { md3 } from 'vuetify/blueprints'
+import { md3 } from "vuetify/blueprints"
 
 export default defineNuxtPlugin((nuxtApp) => {
 	const vuetify = createVuetify({
