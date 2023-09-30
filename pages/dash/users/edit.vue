0c393dfbc45b56fa898f9889a632c23d26df026f
ChunKam Yu <ohhhcky@outlook.com>
2023-09-30 16:21:20 -0700

    feat(dash): add update-pwd

---
+++ pages/dash/users/edit.vue
@@ -0,0 +1,106 @@
+<script setup>
+const localePath = useLocalePath()
+const username = useRoute().query.username
+if (process.client) {
+	const token = sessionStorage.getItem("_cransurvey_token")
+	const username = sessionStorage.getItem("_cransurvey_usr")
+	if (!token || !username) {
+		navigateTo(localePath("/sign-in"))
+	} else if (!sessionStorage.getItem("_cransurvey_token_lock")) {
+		$fetch("/api/usr/token", {
+			method: "POST",
+			body: JSON.stringify({
+				token: token,
+			}),
+		}).then((rsp) => {
+			if (rsp.code == 0) {
+				sessionStorage.setItem("_cransurvey_token_lock", true)
+			} else {
+				sessionStorage.removeItem("_cransurvey_token")
+				sessionStorage.removeItem("_cransurvey_usr")
+				navigateTo(localePath("/sign-in"))
+			}
+		})
+	}
+}
+import "~/src/styles/dash.css"
+</script>
+<template>
+    <h1 class="text-h4">{{ $t("editUser.editUser", { username, }) }}</h1>
+	<div class="mainGroup">
+        <v-card variant="outlined">
+            <v-card-title>{{ $t("editUser.password", { username, }) }}</v-card-title>
+            <v-card-text style="padding-bottom: 0;">
+                <v-text-field :label="$t('signup.password')" type="password" v-model="password"></v-text-field>
+                <v-text-field :label="$t('editUser.enter_password', { username, })" type="password" v-model="newPassword"></v-text-field>
+		        <v-text-field :label="$t('editUser.enter_password_re', { username, })" type="password" v-model="newPasswordRe"></v-text-field>
+                <v-card-actions style="padding-top: 0;">
+                    <v-spacer></v-spacer>
+                    <v-btn variant="text" @click="updatePwd">{{ $t("editUser.update") }}</v-btn>
+                </v-card-actions>
+            </v-card-text>
+            
+        </v-card>
+    </div>
+</template>
+<script>
+import { useToast } from "vue-toastification"
+
+const toast = useToast()
+const toastCfg = {
+	position: "top-right",
+	timeout: 5000,
+	closeOnClick: true,
+	pauseOnFocusLoss: true,
+	pauseOnHover: true,
+	draggable: false,
+	draggablePercent: 0.6,
+	showCloseButtonOnHover: true,
+	hideProgressBar: true,
+	closeButton: "button",
+	icon: true,
+	rtl: false,
+}
+export default {
+	data() {
+		return {
+			drawer: true,
+			rail: true,
+			password: "",
+            newPassword: "",
+            newPasswordRe: "",
+		}
+	},
+	methods: {
+		async updatePwd() {
+            if (!this.password || !this.newPassword || !this.newPasswordRe) {
+                toast.error(this.$t("signup.invalid_params"), toastCfg)
+                return false
+            }
+            if (this.newPassword != this.newPasswordRe) {
+                toast.error(this.$t("signup.invalid_password"), toastCfg)
+                return false
+            }
+            const updatePwdStatus = await $fetch("/api/usr/update", {
+                method: "POST",
+                body: JSON.stringify({
+                    token: sessionStorage.getItem("_cransurvey_token"),
+                    username: useRoute().query.username,
+                    password: this.password,
+                    newPassword: this.newPassword,
+                    type: "pwd"
+                }),
+            })
+            
+        },
+	},
+	async mounted() {
+	},
+}
+</script>
+<style>
+.mainGroup {
+	margin-top: 20px;
+	padding: 10px;
+}
+</style>
