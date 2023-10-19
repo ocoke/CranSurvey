d7d4850dd0c683431839f6efff9d379e19c856e2
ChunKam Yu <ohhhcky@outlook.com>
2023-10-18 21:00:17 -0700

    feat(dash): add footer

--- app.vue
+++ app.vue
@@ -8,18 +8,23 @@
 					<Drawer />
 					<v-main>
 						<NuxtPage />
+						<Footer style="margin-top: 30px;" />
 					</v-main>
 				</v-layout>
 			</v-card>
+			
 		</div>
 		<div v-else>
 			<NuxtLoadingIndicator />
 			<!-- here -->
 			<NuxtPage />
+			<Footer style="margin-top: 60px;"/>
 		</div>
+		
 	</NuxtLayout>
 </template>
 <script setup>
 import "~/src/styles/global.css"
 import Drawer from "~/pages/dash/Drawer.vue"
+import Footer from '~/pages/Footer.vue'
 </script>
---
+++ pages/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        <p>&copy; 2023 <NuxtLink style="color: unset; text-decoration: underline;" to="https://github.com/ocoke/CranSurvey">CranSurvey</NuxtLink>.</p>
+        <p v-show="useI18n().locale.value != 'en-US'">{{ $t('footer.translate') }}</p>
+    </footer>
+</template>
+<style>
+footer {
+	margin: 0 auto;
+	text-align: center;
+	padding: 10px;
+	/* border-top: 1px solid #eee; */
+	opacity: .8;
+}
+</style>
