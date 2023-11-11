af36b5a2a437b81c79419ee65e58e9e60c86011f
Jun Y <ohhhcky@outlook.com>
2023-11-11 10:10:14 -0800

    feat(server): try to get users' ip

--- server/api/ip.ts
+++ server/api/ip.ts
@@ -1,5 +1,20 @@
 import requestIp from "request-ip"
 export default eventHandler(async (event) => {
 	const clientIp = requestIp.getClientIp(event.node.req)
-    return clientIp
+	if (!clientIp) {
+		return "Could not get client IP"
+	}
+	const geoip: object = await $fetch("https://api.ip.sb/geoip/" + clientIp)
+	const city = geoip["city"] || "Unknown"
+	const country = geoip["country"] || "Unknown"
+	const region = geoip["region"] || "Unknown"
+	const country_code = geoip["country_code"] || "Unknown"
+	const region_code = geoip["region_code"] || "Unknown"
+	return {
+		clientIp,
+		city,
+		country,
+		region,
+		country_code,
+	}
 })
